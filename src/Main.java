cb8d37c186cfa3c5b3c032d90d3db6c710c9981b
Rebecca Manhem <rebecca.manhem@gmail.com>
2021-04-09 14:06:19 +0200

    message1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,27 @@ import java.util.stream.Collectors;
 
 public class Main {
 
+    static int a = 5;
+    static int x = 1;
+
     public static void main(String[] args) {
+
+        int returnedValue = increaseInt(a);
+        System.out.println(a);
+
+
+    }
+
+    private static int increaseInt(int plus) {
+
+        for (int i = 1; i <= x; i++) {
+            a = (a + 1);
+        }
+        int b = a;
+        return b;
+    }
+
+        /*
         List<String> stringList = List.of("Avatar", "Captain America", "The avengers");
 
         List<String> sortedList = sortList(stringList);
@@ -17,5 +37,13 @@ public class Main {
                 .collect(Collectors.toList());
     }
 
+    private static List<String> toCapitalLetters (List<String> movies) {
+        return  movies.stream()
+    }
+ /
+
+         */
+
 }
+
 
